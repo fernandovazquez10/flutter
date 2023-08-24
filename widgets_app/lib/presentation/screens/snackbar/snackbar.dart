0ff43c7981a7e16916906f2b1378cf550a886dc9
Alonso <f.vazquez151000@gmail.com>
2023-08-24 00:28:43 -0600

    :construction: New screens

---
+++ widgets_app/lib/presentation/screens/snackbar/snackbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SnackbarScreen extends StatelessWidget {
+
+  static const name = 'snackbar';
+
+  const SnackbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
