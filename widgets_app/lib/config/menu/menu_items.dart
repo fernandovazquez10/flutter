1f727017fc865c00b6ac1e910183c93f1ebadb58
Alonso <f.vazquez151000@gmail.com>
2023-08-24 22:32:36 -0600

    :white_check_mark: Progress indicator

--- widgets_app/lib/config/menu/menu_items.dart
+++ widgets_app/lib/config/menu/menu_items.dart
@@ -18,7 +18,25 @@ class MenuItem {
 
 const appMenuItems = <MenuItem>[
 
-  MenuItem(title: 'Botones', subTitle: 'Varios botones en Flutter', link: '/buttons', icon: Icons.smart_button_outlined),
-  MenuItem(title: 'Tarjetas', subTitle: 'Un contenedor estilizado', link: '/cards', icon: Icons.credit_card),
+  MenuItem(
+    title: 'Botones',
+    subTitle: 'Varios botones en Flutter',
+    link: '/buttons',
+    icon: Icons.smart_button_outlined
+  ),
+
+  MenuItem(
+    title: 'Tarjetas',
+    subTitle: 'Un contenedor estilizado',
+    link: '/cards',
+    icon: Icons.credit_card
+  ),
+
+  MenuItem(
+    title: 'ProgressIndicators',
+    subTitle: 'Generales y controlados',
+    link: '/progress',
+    icon: Icons.refresh_outlined,
+  ),
 
 ];
