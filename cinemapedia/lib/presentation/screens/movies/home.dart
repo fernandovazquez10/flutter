55adf12e26941feef29d5950b2c6e190ee075139
Alonso <f.vazquez151000@gmail.com>
2023-09-02 20:30:18 -0600

    :construction: Movies slideshow

--- cinemapedia/lib/presentation/screens/movies/home.dart
+++ cinemapedia/lib/presentation/screens/movies/home.dart
@@ -1,18 +1,18 @@
-import 'package:cinemapedia/presentation/providers/movies/movies.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends StatelessWidget {
+import 'package:cinemapedia/presentation/providers/movies/movies.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 
+
+class HomeScreen extends StatelessWidget {
   static const name = 'home-screen';
 
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: _HomeView()
-    );
+    return const Scaffold(body: _HomeView());
   }
 }
 
@@ -24,26 +24,25 @@ class _HomeView extends ConsumerStatefulWidget {
 }
 
 class _HomeViewState extends ConsumerState<_HomeView> {
-
   @override
   void initState() {
     super.initState();
 
-    ref.read( nowPlayingMoviesProvider.notifier ).loadNextPage();
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
   }
 
   @override
   Widget build(BuildContext context) {
-    final nowPlayingMovies = ref.watch( nowPlayingMoviesProvider );
-    if (nowPlayingMovies.isEmpty) return const Center(child: CircularProgressIndicator(strokeWidth: 2));
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index){
-        final movie =  nowPlayingMovies[index];
-        return ListTile(
-          title: Text(movie.title),
-        );
-      }
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+    if (nowPlayingMovies.isEmpty) {
+      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+    }
+    return Column(
+      children: [
+        const CustomAppbar(),
+
+        MoviesSlideshow(movies: nowPlayingMovies)
+      ],
     );
   }
 }
