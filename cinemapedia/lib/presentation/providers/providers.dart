0f82c9ea146dcbe85ada26ce6b05d5eefc54105a
Alonso <f.vazquez151000@gmail.com>
2023-09-12 23:36:06 -0600

    :construction: Search movies

--- cinemapedia/lib/presentation/providers/providers.dart
+++ cinemapedia/lib/presentation/providers/providers.dart
@@ -5,3 +5,5 @@ export 'movies/initial_loading.dart';
 export 'movies/movies_repository.dart';
 export 'movies/movies_slideshow.dart';
 export 'movies/movies.dart';
+
+export 'search/search_movies.dart';
--- cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
+++ cinemapedia/lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,8 +1,12 @@
-import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
-
-import 'package:cinemapedia/presentation/delegates/search_movie.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/delegates/search_movie.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
+
+
 
 
 class CustomAppbar extends ConsumerWidget {
@@ -32,13 +36,22 @@ class CustomAppbar extends ConsumerWidget {
                 onPressed: (){
 
                   final movieRepository = ref.read( movieRepositoryProvider );
-
-                  showSearch(
+                  final searchQuery = ref.read( searchQueryProvider );
+                  showSearch<Movie?>(
+                    query: searchQuery,
                     context: context,
                     delegate: SearchMovieDelegate(
-                      searchMovies: movieRepository.searchMovies
+                      searchMovies: (query) {
+                        ref.read( searchQueryProvider.notifier ).update((state) => query);
+                        return movieRepository.searchMovies(query);
+                      }
                     ),
-                  );
+                  ).then((movie) {
+                    if (movie != null){
+                      context.push('/movie/${ movie.id }');
+                    }
+                  });
+                  
                 },
                 icon: const Icon( Icons.search_outlined )
               )
