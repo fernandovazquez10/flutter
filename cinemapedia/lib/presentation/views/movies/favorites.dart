0f7f482d41a5b55804ee944b411b9da6b3771deb
Alonso <f.vazquez151000@gmail.com>
2023-09-28 00:10:57 -0600

    :white_check_mark: No favorites message

--- cinemapedia/lib/presentation/views/movies/favorites.dart
+++ cinemapedia/lib/presentation/views/movies/favorites.dart
@@ -3,6 +3,7 @@ import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 
 class FavoritesView extends ConsumerStatefulWidget {
@@ -41,6 +42,28 @@ class FavoritesViewState extends ConsumerState<FavoritesView> {
   Widget build(BuildContext context) {
 
     final List<Movie> favoritesMovies = ref.watch( favoriteMoviesProvider ).values.toList();
+
+    if ( favoritesMovies.isEmpty ) {
+      final colors = Theme.of(context).colorScheme;
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon( Icons.heart_broken_sharp, size: 60, color: colors.primary ),
+            Text( 'Ohhh no!!', style: TextStyle(fontSize: 30, color: colors.primary)),
+            const Text('No tienes peliculas favoritas', style: TextStyle( fontSize: 20, color: Colors.black45)),
+            
+            const SizedBox(height: 28),
+
+            FilledButton.tonal(
+              onPressed: ()=> context.go('/home/0'), 
+              child: const Text('Empieza a buscar', style: TextStyle(color: Colors.black54)),
+            )
+          ],
+        ),
+      );
+    }
 
     return Scaffold(
       body: MovieMasonry(
