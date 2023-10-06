568aff8785ff76478ba1623ca8a4dbc4c0c818ee
Alonso <f.vazquez151000@gmail.com>
2023-10-06 00:00:03 -0600

    Masonry populars

--- cinemapedia/lib/presentation/views/movies/trends.dart
+++ cinemapedia/lib/presentation/views/movies/trends.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies.dart';
+import 'package:cinemapedia/presentation/widgets/movies/movie_masonry.dart';
 
 
-class TrendsView extends StatelessWidget {
+class TrendsView extends ConsumerStatefulWidget {
   const TrendsView({super.key});
 
   @override
+  TrendsViewState createState() => TrendsViewState();
+}
+
+
+class TrendsViewState extends ConsumerState<TrendsView> {
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder(
-      child: Text('Treanding movies...'),
+
+    final List<Movie> popularMovies = ref.watch( popularMoviesProvider );
+
+    if ( popularMovies.isEmpty ) {
+      return const Center(child: CircularProgressIndicator(strokeWidth: 2));
+    }
+
+    return Scaffold(
+      body: MovieMasonry(
+        loadNextPage: () => ref.read( popularMoviesProvider.notifier ).loadNextPage(),
+        movies: popularMovies,
+      ),
     );
   }
 }
--- cinemapedia/lib/presentation/widgets/movies/movie_poster_link.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_poster_link.dart
@@ -3,25 +3,24 @@ import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-
 class MoviePosterLink extends StatelessWidget {
-
   final Movie movie;
 
-  const MoviePosterLink({
-    super.key,
-    required this.movie
-  });
+  const MoviePosterLink({super.key, required this.movie});
 
   @override
   Widget build(BuildContext context) {
     return FadeInUp(
       child: GestureDetector(
-        onTap: () => context.push('/home/0/movie/${movie.id}') ,
+        onTap: () => context.push('/home/0/movie/${movie.id}'),
         child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: Image.network(movie.posterPath)
-        ),
+            borderRadius: BorderRadius.circular(20),
+            child: FadeInImage(
+              height: 180,
+              fit: BoxFit.cover,
+              placeholder: const AssetImage('assets/loaders/bottle-loader.gif'),
+              image: NetworkImage(movie.posterPath),
+            )),
       ),
     );
   }
