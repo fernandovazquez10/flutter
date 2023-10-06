0cefbbe1f3811e09d5df57858c0d2f97bacf8777
Alonso <f.vazquez151000@gmail.com>
2023-10-06 00:05:10 -0600

    :white_check_mark: Movie cards

--- cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
+++ cinemapedia/lib/presentation/widgets/movies/movie_horizontal_listview.dart
@@ -87,19 +87,17 @@ class _Slide extends StatelessWidget {
         children: [
           SizedBox(
               width: 150,
-              child: FadeInUp(
-                child: GestureDetector(
-                  onTap: () => context.push('/home/0/movie/${movie.id}'),
-                  child: ClipRRect(
-                      borderRadius: BorderRadius.circular(20),
-                      child: FadeInImage(
-                        height: 180,
-                        fit: BoxFit.cover,
-                        placeholder: const AssetImage(
-                            'assets/loaders/bottle-loader.gif'),
-                        image: NetworkImage(movie.posterPath),
-                      )),
-                ),
+              child: GestureDetector(
+                onTap: () => context.push('/home/0/movie/${movie.id}'),
+                child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: FadeInImage(
+                      height: 180,
+                      fit: BoxFit.cover,
+                      placeholder: const AssetImage(
+                          'assets/loaders/bottle-loader.gif'),
+                      image: NetworkImage(movie.posterPath),
+                    )),
               )),
 
           const SizedBox(height: 5),
